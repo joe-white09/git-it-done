cda3376245beec89a9adfb2262a99c62c0b515b5
joe-white09 <joe_white09@yahoo.com>
2022-05-17 16:56:02 -0500

    added link for over 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent= "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warnin container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,6 +21,11 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -51,5 +69,5 @@ var displayIssues = function(issues) {
 };
 };
 
-getRepoIssues("joe-white09/run-buddy");
+getRepoIssues("expressjs/express");
 
