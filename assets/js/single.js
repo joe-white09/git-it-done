098c0de01b7c83360ed9c86c24421ee463296de7
joe-white09 <joe_white09@yahoo.com>
2022-05-18 16:05:39 -0500

    linked pages together to see issues list

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName){
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);   
+    } else{
+        document.location.replace("./index.html");
+    }
+};
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -29,7 +41,7 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -69,5 +81,6 @@ var displayIssues = function(issues) {
 };
 };
 
-getRepoIssues("expressjs/express");
+getRepoName();
+
 
